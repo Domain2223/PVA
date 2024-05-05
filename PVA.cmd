bb9fbe0c9c0080e09cc42ed091d850dd9db5cb5a
danny-zebby <drodriguez125.horizon.csueastbay.edu>
2024-05-04 21:35:51 -0700

    PVA magic completed, having trouble with PVA cmd

--- PVA.cmd
+++ PVA.cmd
@@ -1,7 +1,39 @@
-analyzer CLK A B P0 P1 P2 P3 P4 P5 P6 P7    V0 V1 V2 V3 V4 V5 V6 V7    A0 A1 A2 A3 A4 A5 A6 A7
+analyzer CLK RST  A B P0 P1 P2 P3 P4 P5 P6 P7    V0 V1 V2 V3 V4 V5 V6 V7    A0 A1 A2 A3 A4 A5 A6 A7
 h VDD
 l VSS
 l CLK
+l RST
+s
+s
+s
+s
+s
+s
+s
+h RST
+s
+s
+s
+s
+s
+s
+s
+l RST
+s
+s
+s
+s
+s
+s
+s
+h RST
+s
+s
+s
+s
+s
+s
+s
 # Frequency change
 h A
 h CLK
